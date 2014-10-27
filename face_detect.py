bb9fafd6e45f8171d5c452da37aefadd0e43007d
Software Mechanic <softwaremechanic32@gmail.com>
2014-10-27 23:24:49 +0530

    nges

--- face_detect.py
+++ face_detect.py
@@ -3,25 +3,38 @@ import sys
 
 # Get user supplied values
 imagePath = sys.argv[1]
-cascPath = sys.argv[2]
+class TooManyFacesException(Exception):
+    pass
 
-# Create the haar cascade
-faceCascade = cv2.CascadeClassifier(cascPath)
 
-# Read the image
-image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+class FeatureDetect(self):
+    def __init__(self, image):
+        frontalFaceCascPath = "haarcascade_frontalface_default.xml"
+        # Create the haar cascade
+        faceCascade = cv2.CascadeClassifier(frontalFaceCascPath)
 
-# Detect faces in the image
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30),
-    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
-)
+    def detectFace(self):
+        # Read the image
+        image = cv2.imread(imagePath)
+        grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-print "Found {0} faces!".format(len(faces))
+        # Detect faces in the image
+        faces = faceCascade.detectMultiScale(
+            grayImage,
+            scaleFactor=1.1,
+            minNeighbors=2,
+            minSize=(30, 30),
+            flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+        )
+        assert len(faces) == 1, raise TooManyFacesException({"faces":faces})
+        self.features = {"faceCorners":(x,y,w,h)}
+        print "Found {0} faces!".format(len(faces))
+
+    def detectEyes(self):
+        pass
+
+    def detectLips(self):
+        pass
 
 # Draw a rectangle around the faces
 for (x, y, w, h) in faces:
