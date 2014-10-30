b38949ca847634613fcdba125d9e0ae9810d511f
Software Mechanic <softwaremechanic32@gmail.com>
2014-10-30 16:02:28 +0530

    more stuff.

--- face_detect.py
+++ face_detect.py
@@ -1,9 +1,26 @@
 import cv2
+import hashlib
+import json
+import numpy as np
+import os
+from PIL import Image
 import sys
+import tornado
+from tornado.options import define, options
+from tornado.web import RequestHandler, Application
 
+# Custom backend account settings
+sys.path.append("/home/anand/Downloads/devbox_configs/")
+import backend
+
+define('debug', default=1, help='hot deployment. use in dev only', type=int)
+define('port', default=8000, help='run on the given port', type=int)
+define('imageFolder', default='./uploadedImages', help = 'folder to store uploaded images', type=str)
+
+# FACEDETECT_IMG_HASHES = 'facedetect:img:hashes'
+SET_IMG_HASHES = 'set:img:hashes'
 class TooManyFacesException(Exception):
     pass
-
 
 class FeatureDetect(object):
     def __init__(self, image):
@@ -19,6 +36,7 @@ class FeatureDetect(object):
         self.noseCascade = cv2.CascadeClassifier(self.noseCascPath)
         self.image = image
         self.grayImage = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        self.features = dict()
 
     def detectFace(self):
 
@@ -31,18 +49,18 @@ class FeatureDetect(object):
             flags = cv2.cv.CV_HAAR_SCALE_IMAGE
         )
 #        assert len(faces) == 1, raise TooManyFacesException({"faces":faces})
-        self.features = {"faceCorners":self.faces[0]}
+        self.features.update({"faceCorners":str(self.faces[0])})
 
     def detectNose(self):
         self.noses = self.noseCascade.detectMultiScale(
             self.grayImage,
             scaleFactor=1.1,
             minNeighbors=2,
-            minSize=(30, 30),
+            minSize=(25, 15),
             flags = cv2.cv.CV_HAAR_SCALE_IMAGE
         )
 #        assert len(self.noses) == 1, raise TooManyFacesException({"noses":self.noses})
-        self.features = {"noseCorners":self.noses[0]}
+        self.features.update({"noseCorners":str(self.noses[0])})
 
     def detectLips(self):
         # Detect faces in the image
@@ -50,11 +68,11 @@ class FeatureDetect(object):
             self.grayImage,
             scaleFactor=1.1,
             minNeighbors=2,
-            minSize=(30, 30),
+            minSize=(25, 15),
             flags = cv2.cv.CV_HAAR_SCALE_IMAGE
         )
 #        assert len(self.lips) == 1, raise TooManyFacesException({"faces":self.lips})
-        self.features = {"lipCorners":self.lips[0]}
+        self.features.update({"lipCorners":str(self.lips[0])})
 
     def detectEyes(self):
         # Detect faces in the image
@@ -66,30 +84,70 @@ class FeatureDetect(object):
             flags = cv2.cv.CV_HAAR_SCALE_IMAGE
         )
 #        assert len(self.eyes) == 1, raise TooManyFacesException({"faces":self.eyes})
-        self.features = {"eyeCorners":self.eyes[0]}
+        self.features.update({"eyeCorners":str(self.eyes[0])})
+
+class Application(Application):
+    #  """
+    #  >>> import requests
+    #  >>> requests.post("/shorten", params={"orig_url":"http://google.com"})
+    #  >>> resp = requests.get("/shorten", params={"short_url": "265477614567132497141480353139365708304L"})
+    #  >>> assert resp.url=="http://google.com"
+    #  """
+    def __init__(self):
+        handlers = [
+                (r'/facedetect', FaceDetectHandler),
+                ]
+        settings = dict(
+            autoescape=None,  # tornado 2.1 backward compatibility
+            debug=options.debug,
+            gzip=True,
+            )
+        settings.update({'static_path':'./static'})
+        tornado.web.Application.__init__(self, handlers, **settings)
+        if not os.path.exists(options.imageFolder):
+            os.makedirs(options.imageFolder)
+
+class FaceDetectHandler(RequestHandler):
+    def get(self):
+        self.render('static/imageupload.html')
+
+    def post(self, image=None):
+        # Read the image
+        imgBytes = self.request.files.get('file_inp')[0].body
+        imgFileName = self.request.files.get('file_inp')[0].filename
+        imgHash = hashlib.sha512(imgBytes).hexdigest()
+        imgNew = backend.redisConn.sismember(SET_IMG_HASHES, imgHash)
+        #backend.redislabs.pfadd(FACEDETECT_IMG_HASHES)
+        imgNparr = np.fromstring(imgBytes, np.uint8)
+        imgType = imgFileName.split('.')[1]
+        imgType = '.jpg'
+        image = cv2.imdecode(imgNparr, cv2.CV_LOAD_IMAGE_COLOR)
+        if imgNew:
+            cv2.imwrite(imgHash + imgType, image)
+            backend.redisConn.linsert(imgHash)
+        FD = FeatureDetect(image)
+        FD.detectFace()
+        FD.detectEyes()
+        FD.detectLips()
+        self.finish(json.dumps(FD.features))
+        # Draw a rectangle around the faces
+        # print "Found {0} faces!".format(len(FD.faces))
+        # for (x, y, w, h) in FD.faces:
+        #     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        # print "Found {0} eyes".format(len(FD.eyes))
+        # for (x, y, w, h) in FD.eyes:
+        #     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        # print "Found {0} lips".format(len(FD.lips))
+        # for (x, y, w, h) in FD.lips:
+        #     cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
 def main():
-    # Get user supplied values
-    imagePath = sys.argv[1]
-    # Read the image
-    image = cv2.imread(imagePath)
-    FD = FeatureDetect(image)
-    FD.detectFace()
-    FD.detectEyes()
-    FD.detectLips()
+    tornado.options.parse_command_line()
+    app = Application()
 
-    # Draw a rectangle around the faces
-    print "Found {0} faces!".format(len(FD.faces))
-    for (x, y, w, h) in FD.faces:
-        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-    for (x, y, w, h) in FD.eyes:
-        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), 2)
-
-    for (x, y, w, h) in FD.lips:
-        cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    cv2.imshow("Faces found", image)
-    cv2.waitKey(0)
+    app.listen(options.port, xheaders=True)
+    loop = tornado.ioloop.IOLoop.instance()
+    loop.start()
 
 if __name__ == "__main__":
     main()
