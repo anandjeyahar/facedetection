3cfac556de86832a8a208c24805f236d92686853
Software Mechanic <softwaremechanic32@gmail.com>
2014-11-14 18:52:25 +0530

    more fixes

--- backend.py
+++ backend.py
@@ -1,8 +1,20 @@
 import os
-import psychopg2
+import psycopg2
+import urlparse
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["POSTGRES_HEROKU_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
 
 redistogourl = os.getenv('REDISTOGOURL')
 assert redistogourl
 redisConn = redis.from_url(redistogourl, port=6379)
 pgsqlurl = os.getenv('POSTGRES_HEROKU_URL')
-postgreConn = psychopg2.connect(pgsqlurl)
+postgreConn = psycopg2.connect(pgsqlurl)
