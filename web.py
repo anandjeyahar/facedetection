00be76686ae745172adf231ff86a499971d084e6
Software Mechanic <softwaremechanic32@gmail.com>
2014-11-26 12:47:05 +0530

    more stuff.

--- web.py
+++ web.py
@@ -1,4 +1,3 @@
-import bottle
 import cv2
 import sys
 # Custom backend account settings
@@ -15,44 +14,44 @@ define('debug', default=1, help='hot deployment. use in dev only', type=int)
 define('port', default=8888, help='run on the given port', type=int)
 define('imgFolder', default='./uploadedImages', help = 'folder to store uploaded images', type=str)
 
-@bottle.get('/')
-def facedetect():
-    return bottle.template('static/imageupload.html')
+class FaceDetectHandler(RequestHandler):
+    def get(self):
+        return self.render('static/imageupload.html')
 
-@bottle.post('/')
-def facedetect():
-    # Read the image
-    request = bottle.request
-    imgFd = request.files.get('file_inp')
-    imgFileName = imgFd.filename
-    imgName, imgType = imgFileName.split('.')
-    imgType = '.jpg'
-    imgFullPath = os.path.join(options.imgFolder, imgName + imgType)
-    imgFd.save(imgFullPath)
-    # imgHash = hashlib.sha512(imgBytes).hexdigest()
-    # imgNew = not backend.redisConn.sismember(fdmod.SET_IMG_HASHES, imgHash)
-    # backend.redislabs.pfadd(fdmod.SET_IMG_HASHES)
-    image = cv2.imread(imgFullPath)
-    imgNew = True
-    if imgNew:
-        cv2.imwrite(imgFullPath, image)
-        #backend.redisConn.sadd(fdmod.SET_IMG_HASHES, imgHash)
-    FD = fdmod.FeatureDetect(image)
-    FD.detectFace()
-    FD.detectEyes()
-    FD.detectLips()
-    return bottle.template('static/areaselect.html',imgPath=imgFullPath )
-    #self.finish(json.dumps(FD.features))
-    # Draw a rectangle around the faces
-    # print "Found {0} faces!".format(len(FD.faces))
-    # for (x, y, w, h) in FD.faces:
-    #     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    # print "Found {0} eyes".format(len(FD.eyes))
-    # for (x, y, w, h) in FD.eyes:
-    #     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), 2)
-    # print "Found {0} lips".format(len(FD.lips))
-    # for (x, y, w, h) in FD.lips:
-    #     cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    def post(self):
+        # Read the image
+        request = self.request
+        imgFd = request.files.get('file_inp')
+        imgFileName = imgFd.filename
+        imgName, imgType = imgFileName.split('.')
+        imgType = '.jpg'
+        imgFullPath = os.path.join(options.imgFolder, imgName + imgType)
+        imgFd.save(imgFullPath)
+        # imgHash = hashlib.sha512(imgBytes).hexdigest()
+        # imgNew = not backend.redisConn.sismember(fdmod.SET_IMG_HASHES, imgHash)
+        # backend.redislabs.pfadd(fdmod.SET_IMG_HASHES)
+        image = cv2.imread(imgFullPath)
+        imgNew = True
+        if imgNew:
+            cv2.imwrite(imgFullPath, image)
+            #backend.redisConn.sadd(fdmod.SET_IMG_HASHES, imgHash)
+        FD = fdmod.FeatureDetect(image)
+        FD.detectFace()
+        import pdb; pdb.set_trace()
+        FD.detectEyes()
+        FD.detectLips()
+        return self.render('static/areaselect.html',imgPath=imgFullPath )
+        #self.finish(json.dumps(FD.features))
+        # Draw a rectangle around the faces
+        # print "Found {0} faces!".format(len(FD.faces))
+        # for (x, y, w, h) in FD.faces:
+        #     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        # print "Found {0} eyes".format(len(FD.eyes))
+        # for (x, y, w, h) in FD.eyes:
+        #     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        # print "Found {0} lips".format(len(FD.lips))
+        # for (x, y, w, h) in FD.lips:
+        #     cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
 class Application(Application):
     #  """
@@ -77,7 +76,9 @@ class Application(Application):
 
 def main():
     tornado.options.parse_command_line()
-    bottle.run(host='0.0.0.0', port=options.port, debug=True)
+    App = Application()
+    App.listen(address='0.0.0.0', port=options.port, xheaders=True)
+    tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == "__main__":
     main()
