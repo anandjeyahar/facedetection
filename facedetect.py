208faace5f34b24f3fa027820578bb73848c2825
Software Mechanic <softwaremechanic32@gmail.com>
2014-12-13 09:08:15 +0530

    api with HP ondemand

--- facedetect.py
+++ facedetect.py
@@ -10,38 +10,50 @@ from tornado.options import define, options
 
 FACEDETECT_IMG_CNT = 'facedetect:img:cnt'
 FACEDETECT_IMG_HASHES = 'facedetect:img:hashes'
+CLASSIFICATIONS = ['opencv-haarcascade', 'HP-IDOL']
 
 class TooManyFacesException(Exception):
     pass
 
 class FeatureDetect(object):
-    def __init__(self, image):
-        # TODO: add support for multiple training files and going through them one by one.
-        self.frontalFaceCascPath = "haarcascade_frontalface_default.xml"
-        self.mouthCascPath = "Mouth.xml"
-        self.noseCascPath = "Nariz.xml"
-        self.eyeCascPath = "haarcascades/haarcascade_eye.xml"
-        # Create the haar cascade
-        self.faceCascade = cv2.CascadeClassifier(self.frontalFaceCascPath)
-        self.mouthCascade = cv2.CascadeClassifier(self.mouthCascPath)
-        self.eyeCascade = cv2.CascadeClassifier(self.eyeCascPath)
-        self.noseCascade = cv2.CascadeClassifier(self.noseCascPath)
+    self.HpIDOLOnDemandAsyncUrl = 'https://api.idolondemand.com/1/api/async/detectfaces/v1'
+    self.HpIDOLOnDemandSyncUrl = 'https://api.idolondemand.com/1/api/sync/detectfaces/v1'
+    def __init__(self, image, classifier=None):
+        assert classifier in CLASSIFICATIONS, logger.error('unknown classifier')
+        self.classifier = CLASSIFICATIONS[0]
+        if classifier == CLASSIFICATIONS[0]:
+            # TODO: add support for multiple training files and going through them one by one.
+            self.frontalFaceCascPath = "haarcascade_frontalface_default.xml"
+            self.mouthCascPath = "Mouth.xml"
+            self.noseCascPath = "Nariz.xml"
+            self.eyeCascPath = "haarcascades/haarcascade_eye.xml"
+            # Create the haar cascade
+            self.faceCascade = cv2.CascadeClassifier(self.frontalFaceCascPath)
+            self.mouthCascade = cv2.CascadeClassifier(self.mouthCascPath)
+            self.eyeCascade = cv2.CascadeClassifier(self.eyeCascPath)
+            self.noseCascade = cv2.CascadeClassifier(self.noseCascPath)
         self.image = image
         self.grayImage = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
         self.features = dict()
 
     def detectFace(self):
-
         # Detect faces in the image
-        self.faces = self.faceCascade.detectMultiScale(
-            self.grayImage,
-            scaleFactor=1.1,
-            minNeighbors=2,
-            minSize=(30, 30),
-            flags = cv2.cv.CV_HAAR_SCALE_IMAGE
-        )
+        if self.classifier == CLASSIFICATIONS[0]:
+            self.faces = self.faceCascade.detectMultiScale(
+                self.grayImage,
+                scaleFactor=1.1,
+                minNeighbors=2,
+                minSize=(30, 30),
+                flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+                )
+        else:
+            args = { 'apikey': backend.topCoderHpIDOLOnDemandApiKey,
+                     'text': 'text',
+                     'highlight_expression': 'link',
 
-#        assert len(faces) == 1, raise TooManyFacesException({"faces":faces})
+                     }
+            resp = request.get(self.HpIDOLOnDemandSyncUrl, args)
+            logger.info(resp)
         if len(self.faces) > 0:
             self.features.update({"faceCorners":str(self.faces[0])})
         else:
